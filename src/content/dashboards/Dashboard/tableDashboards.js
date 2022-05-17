103012a53eb3626e6ef0ff24d8a20b30d314c579
suwan jeenyee <suwan.j@swiftlet.co.th>
2022-05-17 23:51:43 +0700

    ทำปุ่มให้เด็กมันดู

--- src/content/dashboards/Dashboard/tableDashboards.js
+++ src/content/dashboards/Dashboard/tableDashboards.js
@@ -55,19 +55,39 @@
 //   );
 // }
 
-
-
 import * as React from 'react';
-import { DataGrid, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid';
-import Paper from '@mui/material/Paper';
+import { DataGrid } from '@mui/x-data-grid';
+import { Paper, Button } from '@mui/material';
 
-const columns = [  
-  { field: 'id', headerName: '#', minWidth: 20, flex:1},
-  { field: 'agentname', headerName: 'Agentname', minWidth: 330, flex:1 },
-  { field: 'datetimes', headerName: 'Datetime', minWidth: 330, flex:1},
-  { field: 'score1', headerName: 'Score1', minWidth: 100 , flex:1,},
-  { field: 'score2', headerName: 'Score2', minWidth: 100 , flex:1,},
-  { field: 'score3', headerName: 'Score3', minWidth: 100 , flex:1,},
+const columns = [
+  { field: 'id', headerName: '#', minWidth: 20, flex: 1 },
+  { field: 'agentname', headerName: 'Agentname', minWidth: 330, flex: 1 },
+  { field: 'datetimes', headerName: 'Datetime', minWidth: 330, flex: 1 },
+  { field: 'score1', headerName: 'Score1', minWidth: 100, flex: 1 },
+  { field: 'score2', headerName: 'Score2', minWidth: 100, flex: 1 },
+  { field: 'score3', headerName: 'Score3', minWidth: 100, flex: 1 },
+  {
+    field: 'action',
+    headerName: 'Action',
+    sortable: false,
+    renderCell: (params) => {
+      const onClick = (e) => {
+        e.stopPropagation(); // don't select this row after clicking
+        const { api } = params;
+        const thisRow = {};
+
+        api
+          .getAllColumns()
+          .forEach(
+            (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+          );
+
+        console.log(JSON.stringify(thisRow, null, 3));
+      };
+
+      return <Button onClick={onClick}>Click</Button>;
+    }
+  }
 ];
 
 // function CustomToolbar() {
@@ -81,29 +101,29 @@ const columns = [
 export default function dashboardTable(props) {
   const { data } = props;
   const rows = data;
-  rows.forEach((element,index) => {
-    element.id = Number(++index)
+  rows.forEach((element, index) => {
+    element.id = Number(++index);
   });
   const [pageSize, setPageSize] = React.useState(10);
   return (
     <>
-     <Paper sx={{ width: '100%', mb: 2 }}>
-     <div style={{ width: '100%' }}>
-      <DataGrid
-        alignItems="flex-center"
-        rows={rows}
-        columns={columns}
-        pageSize={pageSize}
-        rowsPerPageOptions={[10, 20, 50]}
-        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-        autoHeight
-        disableSelectionOnClick 
-        // components={{
-        //   Toolbar: CustomToolbar,
-        // }}     
-         />
-    </div>
-    </Paper>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <div style={{ width: '100%' }}>
+          <DataGrid
+            alignItems="flex-center"
+            rows={rows}
+            columns={columns}
+            pageSize={pageSize}
+            rowsPerPageOptions={[10, 20, 50]}
+            onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+            autoHeight
+            disableSelectionOnClick
+            // components={{
+            //   Toolbar: CustomToolbar,
+            // }}
+          />
+        </div>
+      </Paper>
     </>
   );
 }
